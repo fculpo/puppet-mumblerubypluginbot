8e700bb2d21e09475e9a08d145e859f7ae6c2aa8
Fabien Culpo <fabien.culpo@gmail.com>
2016-01-07 16:38:42 +0100

    expose some more params for mumble

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,16 @@
 class mumble_ruby_bot(
-  $username                  = $::mumble_ruby_bot::params::username,
-  $ruby_version              = $::mumble_ruby_bot::params::ruby_version,
-  $celt_version              = $::mumble_ruby_bot::params::celt_version,
-  $celt_rubygem_version      = $::mumble_ruby_bot::params::celt_rubygem_version,
-  $mumble_rubygem_version    = $::mumble_ruby_bot::params::mumble_rubygem_version,
-  $opus_rubygem_version      = $::mumble_ruby_bot::params::opus_rubygem_version,
-  $quality_bitrate           = $::mumble_ruby_bot::params::quality_bitrate,
+  $username                   = $::mumble_ruby_bot::params::username,
+  $ruby_version               = $::mumble_ruby_bot::params::ruby_version,
+  $celt_version               = $::mumble_ruby_bot::params::celt_version,
+  $celt_rubygem_version       = $::mumble_ruby_bot::params::celt_rubygem_version,
+  $mumble_rubygem_version     = $::mumble_ruby_bot::params::mumble_rubygem_version,
+  $opus_rubygem_version       = $::mumble_ruby_bot::params::opus_rubygem_version,
+  $quality_bitrate            = $::mumble_ruby_bot::params::quality_bitrate,
+  $mumbleserver_host          = $::mumble_ruby_bot::params::mumbleserver_host,
+  $mumbleserver_port          = $::mumble_ruby_bot::params::mumbleserver_port,
+  $mumbleserver_username      = $::mumble_ruby_bot::params::mumbleserver_username,
+  $mumbleserver_targetchannel = $::mumble_ruby_bot::params::mumbleserver_targetchannel,
+  $mumbleserver_userpassword  = $::mumble_ruby_bot::params::mumbleserver_userpassword
 ) inherits ::mumble_ruby_bot::params
 
 {
