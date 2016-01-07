973ea9da988405da8fa750833531411dd3a00007
Fabien Culpo <fabien.culpo@gmail.com>
2016-01-07 13:58:52 +0100

    no commas for params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,12 @@
 class mumble_ruby_bot::params {
 
-	$username               = 'botmaster',
-  $ruby_version           = '2.3.0',
-  $celt_version           = '0.7.0',
-  $celt_rubygem_version   = '0.0.1',
-  $mumble_rubygem_version = '1.1.2',
-  $opus_rubygem_version   = '1.0.1',
+	$username               = 'botmaster'
+  $ruby_version           = '2.3.0'
+  $celt_version           = '0.7.0'
+  $celt_rubygem_version   = '0.0.1'
+  $mumble_rubygem_version = '1.1.2'
+  $opus_rubygem_version   = '1.0.1'
 
-  $quality_bitrate        = 72000,
+  $quality_bitrate        = 72000
 
 }
